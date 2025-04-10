cb460734f44102dbc8406b400b692e60f5fc3c7b
Leoledes <ledesma.leonardoa@gmail.com>
2025-04-10 19:38:19 -0300

    imagen carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,7 @@
+import CarritoCompras from "../img/carritoCompras.png"
 function CartWidget (){
-    return <img src="" alt="carrito"/>
+
+    return <img src={CarritoCompras} alt="carrito"></img>
 }
 
 export default CartWidget
