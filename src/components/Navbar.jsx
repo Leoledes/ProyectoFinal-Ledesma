abe85268669758f6778f96cc73e5ee766673a5e2
Leoledes <ledesma.leonardoa@gmail.com>
2025-04-10 19:43:25 -0300

    agregado logo en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import logoSassenach from "../img/logoSassenach.png"
 import CartWidget from "./CartWidget"
 
 function Navbar (){
 
     return (
         <div>
+            <img src={logoSassenach} alt="logo Sassenach Farmacia"></img>
             <p>Cuidado Personal</p>
             <p>Belleza</p>
             <p>Farmacia</p>
