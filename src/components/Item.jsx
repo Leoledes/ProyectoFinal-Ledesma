343c2698145fc7801109ecb6a24e5fde0b9e0ad8
Leonardo Ledesma <lledesma@assistcargo.com>
2025-05-04 20:11:30 -0300

    agregado de descripciones y ajustes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,9 @@ const Item =(props)=>{
     return (
         <div className={"card"}>
             <h2>{props.product.title}</h2>
+            <img>{props.product.img}</img>
             <h3>${props.product.price}</h3>
+            <p>{props.product.description}</p>
         </div>
     )
 }
--- src/utils/getProducts.js
+++ src/utils/getProducts.js
@@ -1,14 +1,14 @@
 const products = [
-    {title:"Pasta dental Colgate", price:"7745", id:1},
-    {title:"Shampoo Elvive", price:"5193", id:2},
-    {title:"Toallas humedas Huggies", price:"5489", id:3},
-    {title:"Serum hidratante Isdin", price:"102399", id:4},
-    {title:"Protector Solar Isdin", price:"55630", id:5},
-    {title:"Serum antiedad LaRoche", price:"104727", id:6},
-    {title:"Ibupirac Ibuprofeno", price:"2721", id:7},
-    {title:"Tafirol forte Paracetamol", price:"6536", id:8},
-    {title:"Coltix Hidra lagrimas", price:"7209", id:9},
-    {title:"Melatol Día Relax", price:"8842", id:10}
+    {title:"Pasta dental Colgate", price:"7745", description: "descripcion de producto", id:1},
+    {title:"Shampoo Elvive", price:"5193", description: "descripcion de producto", id:2},
+    {title:"Toallas humedas Huggies", price:"5489", description: "descripcion de producto", id:3},
+    {title:"Serum hidratante Isdin", price:"102399", description: "descripcion de producto", id:4},
+    {title:"Protector solar Isdin", price:"55630", description: "descripcion de producto", id:5},
+    {title:"Serum antiedad LaRoche", price:"104727", description: "descripcion de producto", id:6},
+    {title:"Ibupirac Ibuprofeno", price:"2721", description: "descripcion de producto", id:7},
+    {title:"Tafirol forte Paracetamol", price:"6536", description: "descripcion de producto", id:8},
+    {title:"Coltix Hidra lagrimas", price:"7209", description: "descripcion de producto", id:9},
+    {title:"Melatol Día Relax", price:"8842", description: "descripcion de producto", id:10}
 ]
 
 export const getProducts = () => {
