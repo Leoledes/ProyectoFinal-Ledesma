1e3008dbeb9f2d7992be7d60289e208b82ce3676
Leonardo Ledesma <lledesma@assistcargo.com>
2025-05-23 15:33:00 -0300

    link de carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom"
 import CarritoCompras from "../img/carritoCompras.png"
 
 function CartWidget (){
     return (
+      <Link to="/cart">
         <div className={"cartcontainer"}>
           <img className={"carticon"} src={CarritoCompras} alt="Carrito"></img>
           <span className={"cartcount"}>0</span>
         </div>
+      </Link>
     )
 }
 
