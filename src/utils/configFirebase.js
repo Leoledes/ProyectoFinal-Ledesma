fa448f260e1866f9629f30a4aabc40c0be159e8c
Leonardo Ledesma <lledesma@assistcargo.com>
2025-05-27 15:01:32 -0300

    agregado firebase

---
+++ src/utils/configFirebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCw6U7g6W4w8vufowslCmZQaYT7wMb39vY",
+  authDomain: "sassenach-boticario.firebaseapp.com",
+  projectId: "sassenach-boticario",
+  storageBucket: "sassenach-boticario.firebasestorage.app",
+  messagingSenderId: "85995608452",
+  appId: "1:85995608452:web:c5bf9d03aff1534c6365d1"
+};
+
+
+const app = initializeApp(firebaseConfig);
