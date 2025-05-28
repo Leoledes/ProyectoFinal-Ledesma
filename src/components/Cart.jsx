f9f3a011a646b1ec9848099db44e212fca693179
Leonardo Ledesma <lledesma@assistcargo.com>
2025-05-28 00:44:33 -0300

    Agregado Checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { cartContext } from "./CartContext";
+import { Link } from "react-router-dom"
 
 const Cart = () => {
     const {cart, cleanCart, removeItem, getTotalPrice} = useContext(cartContext)
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,11 +1,65 @@
+import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore"
+import { useContext } from "react"
+import { cartContext } from "./CartContext"
+import { useNavigate } from "react-router-dom"
+
 const Checkout = () => {
+    const { getTotalPrice, cart, cleanCart } = useContext(cartContext)
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState(0)
+    const [address, setAddress] = useState("")
+
+    const navigate = useNavigate()
+    const handleCreateOrder = () => {
+        const db = getFirestore()
+        const collectionRef = collection(db, "orders")
+
+        const orderData = {
+            buyer: {
+                name,
+                email,
+                phone,
+                address
+            },
+            total: getTotalPrice(),
+            items: cart,
+            date: serverTimestamp(),
+        }
+
+        addDoc(collectionRef, orderData)
+        .then((response) => {
+            alert(`Tu orden fue creada correctamente con el id: ${response.id}`)
+            cleanCart()
+            navigate("/")
+        })
+        .catch(()=>{
+            console.log("Error al crear la orden")
+        })
+    }
+    
+    const handleChangeName = (evt) => {
+        setName(evt.target.value)
+    }
+    const handleChangeEmail = (evt) => {
+        setEmail(evt.target.value)
+    }
+    const handleChangePhone = (evt) => {
+        setPhone(evt.target.value)
+    }
+    const handleChangeAddress = (evt) => {
+        setAddress(evt.target.value)
+    }
+
     return (
         <div>
-            <input placeholder="Nombre"></input>
-            <input placeholder="Correo electronico"></input>
-            <input placeholder="Telefono"></input>
-            <input placeholder="Direccion"></input>
-            <button>Realizar pedido</button>
+            <input placeholder="Nombre" onChange={handleChangeName}></input>
+            <input placeholder="Correo electronico" onChange={handleChangeEmail}></input>
+            <input placeholder="Telefono" onChange={handleChangePhone}></input>
+            <input placeholder="Direccion" onChange={handleChangeAddress}></input>
+
+            <button onClick={handleCreateOrder}>Realizar pedido</button>
         </div>
     )
 }
